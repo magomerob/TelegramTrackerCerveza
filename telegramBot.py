7686b34e715194206b9fcafcac50ba8b8bfd2b17
magomerob <magomerob@unirioja.es>
2024-01-13 21:22:08 +0100

    version correcta

--- telegramBot.py
+++ telegramBot.py
@@ -1,48 +1,103 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+#!/usr/bin/env python
+# pylint: disable=unused-argument
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Basic example for a bot that uses inline keyboards. For an in-depth explanation, check out
+ https://github.com/python-telegram-bot/python-telegram-bot/wiki/InlineKeyboard-Example.
+"""
+import logging
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
+
 from addtosheet import addRow
 
-async def cana(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    addRow("Caña")
-    await update.message.reply_text(f'Que aproveche, {update.effective_user.first_name}!')
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
-async def litrona(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    addRow("Litrona")
-    await update.message.reply_text(f'Que aproveche, {update.effective_user.first_name}!')
+logger = logging.getLogger(__name__)
 
-async def pinta(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    addRow("Pinta")
-    await update.message.reply_text(f'Que aproveche, {update.effective_user.first_name}!')
 
-async def corto(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    addRow("Corto")
-    await update.message.reply_text(f'Que aproveche, {update.effective_user.first_name}!')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends a message with three inline buttons attached."""
+    keyboard = [
+        [
+            InlineKeyboardButton("Caña", callback_data="Caña"),
+            InlineKeyboardButton("Litrona", callback_data="Litrona"),
+        ],
+        [
+            InlineKeyboardButton("Pinta", callback_data="Pinta"),
+            InlineKeyboardButton("Corto", callback_data="Corto"),
+        ],
+        [
+            InlineKeyboardButton("Lata", callback_data="Lata"),
+            InlineKeyboardButton("Botellín", callback_data="Botellín"),
+        ],
+        
+    ]
 
-async def lata(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    addRow("Lata")
-    await update.message.reply_text(f'Que aproveche, {update.effective_user.first_name}!')
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
-async def botellin(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    addRow("Botellin")
-    await update.message.reply_text(f'Que aproveche, {update.effective_user.first_name}!')
+    await update.message.reply_text("Que vas a beber?:", reply_markup=reply_markup)
 
-def main():
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    await query.answer()
+
+    keyboard = [
+    [
+        InlineKeyboardButton("Caña", callback_data="Caña"),
+        InlineKeyboardButton("Litrona", callback_data="Litrona"),
+    ],
+    [
+        InlineKeyboardButton("Pinta", callback_data="Pinta"),
+        InlineKeyboardButton("Corto", callback_data="Corto"),
+    ],
+    [
+        InlineKeyboardButton("Lata", callback_data="Lata"),
+        InlineKeyboardButton("Botellín", callback_data="Botellín"),
+    ],]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+
+    await query.edit_message_text(text=f"Que aproveche tu: {query.data}", reply_markup=reply_markup)
+
+    print(f"added {query.data}")
+    addRow(str(query.data))
+    start(update, context)
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Displays info on how to use the bot."""
+    await update.message.reply_text("Use /start to test this bot.")
+
+
+def main() -> None:
+    """Run the bot."""
+
     with open('Clave/telegramApi.txt', 'r') as file:
         TOKEN = file.read().strip()
 
-    app = ApplicationBuilder().token(TOKEN).build()
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("cana", cana))
-    app.add_handler(CommandHandler("litrona", litrona))
-    app.add_handler(CommandHandler("pinta", pinta))
-    app.add_handler(CommandHandler("corto", corto))
-    app.add_handler(CommandHandler("lata", lata))
-    app.add_handler(CommandHandler("botellin", botellin))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button))
+    application.add_handler(CommandHandler("help", help_command))
 
-    app.add_handler(CommandHandler("help", help))
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
-    app.run_polling()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
